29b99d10b2dc9f844db9d13ccc9d1224d79ca49d
jose0033 <101925195+jose0033@users.noreply.github.com>
2022-03-26 16:40:49 +0800

    Update app.py

--- app.py
+++ app.py
@@ -12,23 +12,14 @@ def index():
         model1 = joblib.load("CART")
         purchases = float(purchases)
         suppcard = float(suppcard)
-        pred1 = model1.predict([[purchases, suppcard]])
-
-        model2 = joblib.load("RF")
-        purchases = float(purchases)
-        suppcard = float(suppcard)
-        pred2 = model1.predict([[purchases, suppcard]])
-
-        model3 = joblib.load("GB")
-        purchases = float(purchases)
-        suppcard = float(suppcard)
-        pred3 = model1.predict([[purchases, suppcard]])
-
-        return (render_template("index.html", result1="1", result2="1", result3="1"))
-
+        pred1 = model1.predict([[purchases,suppcard]])
+        model2= joblib.load("RF")
+        pred2 = model2.predict([[purchases,suppcard]])
+        model3= joblib.load("GB")
+        pred3 = model3.predict([[purchases,suppcard]])
+        return(render_template("index.html", result1=pred1, result2=pred2, result3=pred3))
     else:
-        return (render_template("index.html", result1="2", result2="2", result3='2'))
-
+        return(render_template("index.html", result1="2", result2="2", result3='2'))
 
 if __name__ == "__main__":
     app.run()
