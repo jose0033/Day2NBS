cdb4a422c90f434b5ad8c934d191e55f53b0b262
jose0033 <101925195+jose0033@users.noreply.github.com>
2022-04-13 14:36:17 +0800

    Update main.py

--- main.py
+++ main.py
@@ -33,6 +33,33 @@ joblib.dump(model, "CART")
 
 from sklearn.ensemble import RandomForestClassifier
 
+from sklearn.model_selection import train_test_split
+
+X_train, X_test, Y_train, Y_test = train_test_split(x,y, random_state=204)
+
+from sklearn import tree
+
+model = tree.DecisionTreeClassifier(random_state=204)
+model.fit(X_train, Y_train)
+
+pred = model.predict(X_train)
+
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(Y_train, pred)
+print(cm)
+
+accuracy = (cm[0,0] + cm[1,1]/(sum(sum(cm))))
+
+print(accuracy)
+
+pred = model.predict(X_test)
+
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(Y_test, pred)
+print(cm)
+accuracy = (cm[0,0] + cm[1,1])/(sum(sum(cm)))
+print(accuracy)
+
 model = RandomForestClassifier(ccp_alpha=0.0384)
 model.fit(x,y)
 pred = model.predict(x)
